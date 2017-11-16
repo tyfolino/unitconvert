60491cbe6937d09357defeaff24c8e1d9fd99905
Tyler Janoski <tpj2104@columbia.edu>
2017-11-16 10:25:32 -0500

    I guess I didn't update something in setup??

---
+++ unitconvert/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "unitconvert",
+    version = "0.0.1",
+    author = "Ty Janoski",
+    packages=['unitconvert'],
+    install_requires=[],
+)
+
+
